d45981a0e4c998b736428e05a1288285ba76b9f5
github username <github email address>
2021-07-02 17:24:25 -0400

    commit tree 388f0c69d7616893ae6737d8084a881c5df972d0

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
-// Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia'];
+// const returnFirstTwoDrivers = (driversArray) => {return driversArray.slice(0, 2)}
+const returnFirstTwoDrivers = drivers => drivers.slice(0,2)
+// const returnLastTwoDrivers = (driversArray) => {return driversArray.slice(2, 4)}
+const returnLastTwoDrivers = drivers => drivers.slice(2,4)
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+// const createFareMultiplier = multiplier => function(fare) { return fare * multiplier }
+
+// const createFareMultiplier = num => function(fare){
+//     return fare * num 
+// }
+
+const createFareMultiplier = num => (fare => fare * num)
+
+const fareDoubler = createFareMultiplier(2);
+const fareTripler = createFareMultiplier(3);
+
+// const selectDifferentDrivers = (driversArray, whatevFunction) => {return whatevFunction(driversArray) }
+const selectDifferentDrivers = (drivers, someFunc) => someFunc(drivers); 
